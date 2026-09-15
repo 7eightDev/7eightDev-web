87b58473241f3e694e39c3abd58ee8f2355caba7
7eightDev <7eightdev@gmail.com>
2026-09-15 10:30:15 +0200

    test(responsive): Lead detail page + dialog E2E (MS-5.3)

--- app/(private)/admin/leads/[id]/page.tsx
+++ app/(private)/admin/leads/[id]/page.tsx
@@ -54,7 +54,7 @@ export default async function LeadDetailPage({
             {lead.companyName}
           </h1>
         </div>
-        <div className="flex items-center gap-2">
+        <div className="flex flex-wrap items-center gap-2">
           <StatusBadge status={lead.status} />
           <LeadSendAuditButton
             leadId={lead.id}
